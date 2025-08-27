71807775bcea3e9860688896c59c9b43984db224
dwis312 <dwi31287@gmail.com>
2025-08-27 20:55:14 +0700

    tugas: Fondasi Java

---
+++ Tahap-1-Fondasi-Java/src/App.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        List<Person> list = new ArrayList<>();
+
+        list.add(new Person("Udin", 34));
+        list.add(new Person("Nana", 24));
+        list.add(new Person("Budi", 20));
+        list.add(new Person("Ani", 24));
+        list.add(new Person("Lisa", 28));
+        list.add(new Person("Joko", 24));
+
+        System.out.println("---------------------------------");
+        System.out.printf("| %-5s | %-10s | %-8s |\n",
+        "No","Nama","Usia");
+        System.out.println("---------------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("| %-5s | %-10s | %-8s |\n",
+            i+1,list.get(i).name,list.get(i).age);
+        }
+        System.out.println("---------------------------------");
+    }
+}
